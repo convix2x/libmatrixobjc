d02dba20bb1f0f2bce4fe6304ffa637c1e000a96
Coni <coni@national.shitposting.agency>
2026-03-22 12:48:16 -0400

    new instance create

---
+++ testclient/TeCliAppDelegate.m
@@ -0,0 +1,14 @@
+#import "TeCliAppDelegate.h"
+#import "TeCliRootViewController.h"
+
+@implementation TeCliAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+	_window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+	_rootViewController = [[UINavigationController alloc] initWithRootViewController:[[TeCliRootViewController alloc] init]];
+	_window.rootViewController = _rootViewController;
+	[_window makeKeyAndVisible];
+	return YES;
+}
+
+@end
